a18c3727742fc15b4c2503c63a142ac15a45852b
Gabriel Marques <marquesgabriel@live.com>
2021-02-22 12:45:42 -0300

    Code comments

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 
+// Tipagem express(tipagem em biblioteca externa): yarn add @types/express -D
+// Conversão/Node: yarn add typescript -D
+// Inicializar TS na aplicação: yarn tsc --init
+// TS-node-dev para converter código em tempo de execução p/ JS: yarn add ts-node-dev -D
+
 const app = express();
 
 /**
+ * HTTP:
  * GET => Busca
  * POST => Salvar
  * PUT => Alterar
@@ -17,11 +23,10 @@ app.get("/", (request, response) => {
 
 // 1 param => Rota(Recurso API)
 // 2 param => (request, response)
-
-
 app.post("/", (request, response) => {
     // Recebeu dados para salvar
     return response.json({message: "Dados salvos com sucesso"})
 })
 
+// Yarn dev configurado no package.json pelo Ts-node-dev
 app.listen(3333, () => console.log("Server is running!"));
