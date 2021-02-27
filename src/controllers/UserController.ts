b2b1263d7f9e65d79159a3d66f007a70e73c79f7
Gabriel Marques <marquesgabriel@live.com>
2021-02-26 21:04:50 -0300

    Validation

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,31 @@
 import { Request, Response } from "express"
 import { getCustomRepository } from "typeorm"
 import { UsersRepository } from "../repositories/UsersRepository"
+import * as yup from "yup"
 
 class UserController {
 
     async create(request: Request, response: Response) {
         const { name, email } = request.body
+
+        const schema = yup.object().shape({
+            name: yup.string().required("Nome é obrigatório"),
+            email: yup.string().email().required("Email incorreto")
+        })
+
+        // if(! (await schema.isValid(request.body))) {
+        //     return response.status(400).json({
+        //         error: "Validation Failed"
+        //     })
+        // }
+
+        try {
+           await schema.validate(request.body, { abortEarly: false})
+        } catch (err) {
+            return response.status(400).json({
+                error: err
+            })
+        }
 
         // criar repositório de usuário para ter acesso a métodos no typeorm
         // const userRepository = getRepository(User)
